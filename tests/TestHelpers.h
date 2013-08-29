2ce0cb740c3667ce4e1babc69327d6257d569221
Peter Kümmel <syntheticpp@gmx.net>
2013-08-29 15:28:50 +0200

    add test for init

--- tests/TestHelpers.h
+++ tests/TestHelpers.h
@@ -2,7 +2,32 @@
 
 #include <QTest>
 
+#include <QDir>
+
 #include "qgitexception.h"
 
 #define TO_STR(s) #s
 #define VALUE_TO_STR(s) TO_STR(s)
+
+bool removeDir(const QString & dirName)
+{
+    bool result = true;
+    QDir dir(dirName);
+
+    if (dir.exists(dirName)) {
+        Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst)) {
+            if (info.isDir()) {
+                result = removeDir(info.absoluteFilePath());
+            }
+            else {
+                result = QFile::remove(info.absoluteFilePath());
+            }
+
+            if (!result) {
+                return result;
+            }
+        }
+        result = dir.rmdir(dirName);
+    }
+    return result;
+}
